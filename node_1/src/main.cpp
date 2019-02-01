43717a3914f19f29254ccdcf295869e9b0ac5dfc
Portfence <alberthavnegjerde@live.com>
2019-02-01 13:32:58 +0100

    tested protobuf generation with cmake, updated CMakeLists.txt

--- node_1/src/main.cpp
+++ node_1/src/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <../build/AddressBook.pb.h>
 
 using std::cout;
 using std::endl;
 
 int main(int argc, char * argv[])
-{
-    cout << "Run: C++ project template" << endl;
-    return 0;
+{  
+  GRPC_Experiment::Person p1;
+  p1.set_name("Albert");
+  p1.set_id(1);
+  cout << "First person name and id: " << p1.name() << ", " << p1.id() << endl;
+
+  GRPC_Experiment::AddressBook b;
+
+  return 0;
 }
